c0b2f02482deb8d73f911f24e2c49e6c771e4337
MOHIT RANA <awesomemohit20@gmail.com>
2024-11-06 17:05:02 +0530

    changed models

--- models/Assignment.js
+++ models/Assignment.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 
 const assignmentSchema = new mongoose.Schema({
-    userId: {
-        type : mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    adminId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    task : {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'accepted', 'rejected'],
-        default: 'pending',
-    },
-}, {timestamps: true});
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  task: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Assignment', assignmentSchema);
--- models/User.js
+++ models/User.js
@@ -2,31 +2,26 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username : {
-        type : String,
-        required : true,
-        unique: true,
-    },
-    password: {
-        type : String,
-        required: true,
-    },
-    role: {
-        type : String,
-        enum: ['user', 'admin'],
-        default: 'user',
-    },
-}, { timestamps: true});
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
 userSchema.pre('save', async function(next) {
-    if(this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
-
-userSchema.methods.comparePassword = async function(candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
